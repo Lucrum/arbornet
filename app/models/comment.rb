c7eff797839c0231c7c807be323e429acb78cb2a
Elton <28992495+Lucrum@users.noreply.github.com>
2023-10-26 19:16:48 -0400

    Presence validations for comments and posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable
   has_many :likes, as: :likable
+
+  validates :content, length: { minimum: 1, message: "Comment is too short" }
+  validates :creator, presence: true
+  validates :commentable_type, inclusion: { in: %w[Post Comment] }
+  validates :commentable_id, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :likes, as: :likable, dependent: :destroy
+
+  validates :content, length: { minimum: 1, message: "Post is too short" }
 end
